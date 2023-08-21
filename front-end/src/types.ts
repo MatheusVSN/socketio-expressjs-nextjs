d782fbf232577e349e3ea3142731aa9c743e112e
Matheus <matheusvsn2005@gmail.com>
2023-08-21 20:18:13 -0300

    Added a few types

--- front-end/src/types.ts
+++ front-end/src/types.ts
@@ -1,4 +1,24 @@
 export interface AuthenticationResponse {
+    message: string
     acessToken: string
     refreshToken: string
+}
+
+export interface Message {
+    id: string
+    userId: string
+    message: string
+    createdAt: string
+    updatedAt: String
+}
+
+export type MessagesListResponse = Message[]
+
+export interface User {
+    id: string
+    username: string
+}
+
+export interface Profile extends User {
+    email: string
 }
