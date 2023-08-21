d8e9d716a1234760b714d497b6e0edc90ef5d87a
Matheus <matheusvsn2005@gmail.com>
2023-08-21 20:17:15 -0300

    custom hook for profile

---
+++ front-end/src/hooks/useCurrentUser.ts
@@ -0,0 +1,55 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { Profile } from "@/types"
+import { fetchWrapper } from "@/lib/fetch"
+import { useRouter } from "next/navigation"
+
+const loadUserInformation = async () => {
+    const profileResponse = await fetchWrapper<{ data: Profile }>("/api/profile", {
+        method: "GET"
+    })
+
+    if (profileResponse.status !== 200) return
+
+    return {
+        id: profileResponse.result.data.id,
+        email: profileResponse.result.data.email,
+        username: profileResponse.result.data.username
+    }
+}
+
+export const useCurrentUser = () => {
+    const [user, setUser] = useState<Profile | null>(null)
+    const [loading, setLoading] = useState<boolean>(true)
+    const router = useRouter()
+
+    const logOut = async () => {
+        await fetchWrapper("/api/logout")
+        setUser(() => null)
+        router.push("/")
+    }
+
+    const getUserInformation = async () => {
+        setLoading(() => true)
+        const userInformation = await loadUserInformation()
+        if (userInformation) {
+            setUser(() => userInformation)
+        }
+        setLoading(() => false)
+    }
+
+    useEffect(() => {
+        (async () => {
+            setLoading(() => true)
+            const userInformation = await loadUserInformation()
+            if (userInformation) {
+                setUser(() => userInformation)
+            }
+            setLoading(() => false)
+        })()
+    }, [])
+
+
+    return { user, loading, logOut, getUserInformation }
+}
